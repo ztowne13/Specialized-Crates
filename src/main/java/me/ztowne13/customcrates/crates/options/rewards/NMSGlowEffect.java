04767dc7cb6111d6f7a0df346fe954b28720bd50
Ztowne13 <ztowne13@gmail.com>
2019-04-26 21:50:53 -0700

    Removed MC 1.7 specific features

--- src/main/java/me/ztowne13/customcrates/crates/options/rewards/NMSGlowEffect.java
+++ src/main/java/me/ztowne13/customcrates/crates/options/rewards/NMSGlowEffect.java
@@ -1,8 +1,5 @@
 package me.ztowne13.customcrates.crates.options.rewards;
 
-import net.minecraft.server.v1_7_R4.NBTTagCompound;
-import net.minecraft.server.v1_7_R4.NBTTagList;
-import org.bukkit.craftbukkit.v1_7_R4.inventory.CraftItemStack;
 import org.bukkit.inventory.ItemStack;
 
 /**
@@ -20,18 +17,19 @@ public class NMSGlowEffect extends GlowEffect
     @Override
     public ItemStack apply()
     {
-        net.minecraft.server.v1_7_R4.ItemStack nmsStack = CraftItemStack.asNMSCopy(stack);
-        NBTTagCompound tag = null;
-        if (!nmsStack.hasTag())
-        {
-            tag = new NBTTagCompound();
-            nmsStack.setTag(tag);
-        }
-        if (tag == null) tag = nmsStack.getTag();
-        NBTTagList ench = new NBTTagList();
-        tag.set("ench", ench);
-        nmsStack.setTag(tag);
-        return CraftItemStack.asCraftMirror(nmsStack);
+//        net.minecraft.server.v1_7_R4.ItemStack nmsStack = CraftItemStack.asNMSCopy(stack);
+//        NBTTagCompound tag = null;
+//        if (!nmsStack.hasTag())
+//        {
+//            tag = new NBTTagCompound();
+//            nmsStack.setTag(tag);
+//        }
+//        if (tag == null) tag = nmsStack.getTag();
+//        NBTTagList ench = new NBTTagList();
+//        tag.set("ench", ench);
+//        nmsStack.setTag(tag);
+//        return CraftItemStack.asCraftMirror(nmsStack);
+        return null;
     }
 
     @Override
