9214a26b022fb46da7a85c992cdd212843564938
Ztowne13 <ztowne13@gmail.com>
2019-08-12 21:03:41 -0700

    Fixed '[' and ']' being in the reward display hologram
    - Sub-stringed the message to exclude the first and last letters

--- src/main/java/me/ztowne13/customcrates/crates/options/CActions.java
+++ src/main/java/me/ztowne13/customcrates/crates/options/CActions.java
@@ -264,7 +264,10 @@ public class CActions extends CSetting
             String msg = placedCrate.getCrates().getCs().getCholoCopy().getRewardHologram();
             if (!msg.equalsIgnoreCase(""))
             {
-                msg = ChatUtils.toChatColor(msg.replace("%reward%", rewards.toString()).replace("%player%", p.getName()));
+//                msg = ChatUtils.toChatColor(msg.replace("%reward%", rewards.toString().replace("[", "").replace("]", ""))).replace("%player%", p.getName()));
+
+                msg = ChatUtils.toChatColor(msg.replace("%reward%", rewards.toString().substring(1, rewards.toString().length() - 1)).replace("%player%", p.getName()));
+
 
                 final DynamicHologram dynamicHologram = placedCrate.getCholo().getDh();
                 dynamicHologram.setDisplayingRewardHologram(true);
