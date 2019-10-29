4b1c84db2408a30af0c6371f865d32025ecf6bc4
Ztowne13 <ztowne13@gmail.com>
2019-10-28 17:45:47 -0700

    Added debug command
    - Added debug command with a few uses in it to help debug

---
+++ src/main/java/me/ztowne13/customcrates/commands/sub/Debug.java
@@ -0,0 +1,81 @@
+package me.ztowne13.customcrates.commands.sub;
+
+import me.ztowne13.customcrates.SpecializedCrates;
+import me.ztowne13.customcrates.commands.Commands;
+import me.ztowne13.customcrates.utils.ChatUtils;
+import me.ztowne13.customcrates.utils.nbt_utils.NBTTagManager;
+import me.ztowne13.customcrates.utils.nbt_utils.NBTTagReflection;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+public class Debug extends SubCommand
+{
+    boolean debug = true;
+
+    public Debug()
+    {
+        super("debug", 1, "");
+    }
+
+    @Override
+    public boolean run(SpecializedCrates cc, Commands cmds, String[] args)
+    {
+        if(cmds.getCmdSender() instanceof Player)
+        {
+            Player player = (Player) cmds.getCmdSender();
+
+            if (debug)
+            {
+                if(args.length == 1)
+                {
+                    ChatUtils.msgInfo(player, "Commands: iteminfo[-,tostring], applytag[{tag id}]");
+                    return true;
+                }
+
+                if (args[1].equalsIgnoreCase("iteminfo"))
+                {
+                    ItemStack stack = player.getItemInHand();
+                    ItemMeta im = stack.getItemMeta();
+
+                    if(args.length == 3 && args[2].equalsIgnoreCase("tostring"))
+                    {
+                        cmds.msg("toString(): " + stack.toString());
+                        cmds.msg("im.toString(): " + im.toString());
+                    }
+
+                    cmds.msg("Type: " + stack.getType().name());
+
+                    cmds.msg("Lore:");
+                    for(String s : im.getLore())
+                        cmds.msg("- " + s);
+
+                    cmds.msg("Enchants: ");
+                    for(Enchantment ench : im.getEnchants().keySet())
+                        cmds.msg("- " + ench.getKey().getKey() + " lvl " + im.getEnchants().get(ench));
+
+                    cmds.msg("Data: " + stack.getDurability());
+                    cmds.msg("NBT Tags:");
+                    for(String s : NBTTagManager.getFrom(stack))
+                        cmds.msg("- " + s);
+
+                }
+                else if(args[1].equalsIgnoreCase("applytag"))
+                {
+                    String tag = args[2];
+                    String id = args[3];
+                    String combined = tag + " " + id;
+
+                    player.setItemInHand(NBTTagReflection.applyTo(player.getItemInHand(), combined));
+                    cmds.msgSuccess("Added tag '" + combined + "'");
+                }
+            }
+            else
+            {
+                ChatUtils.msgError(player, "This command is disabled in non-development builds.");
+            }
+        }
+        return false;
+    }
+}
