72a72664cc1007cf808df0e4c7e63fea72534c76
Ztowne13 <ztowne13@gmail.com>
2019-11-30 00:20:54 -0800

    Updated a few of the placeholders to work for non-prem

--- src/main/java/me/ztowne13/customcrates/interfaces/verification/AntiFraudPlaceholders.java
+++ src/main/java/me/ztowne13/customcrates/interfaces/verification/AntiFraudPlaceholders.java
@@ -1,16 +1,18 @@
 package me.ztowne13.customcrates.interfaces.verification;
 
+import org.bukkit.Bukkit;
+
 public class AntiFraudPlaceholders
 {
-    static String USER = "%%__USER__%%";
-
-    static String RESOURCE = "%%__RESOURCE__%%";
-
-    static String NONCE = "%%__NONCE__%%";
-
-//    static String USER = "TEST-" + Bukkit.getPluginManager().getPlugin("SpecializedCrates").getDescription().getVersion();
+//    static String USER = "%%__USER__%%-DEMO-" + Bukkit.ge;
 //
-//    static String RESOURCE = "TEST-" + Bukkit.getPluginManager().getPlugin("SpecializedCrates").getDescription().getVersion();
+//    static String RESOURCE = "%%__RESOURCE__%%";
 //
-//    static String NONCE = "TEST-" + Bukkit.getPluginManager().getPlugin("SpecializedCrates").getDescription().getVersion();
+//    static String NONCE = "%%__NONCE__%%";
+
+    static String USER = "DEMO1-" + Bukkit.getPluginManager().getPlugin("SpecializedCrates-Demo").getDescription().getVersion();
+
+    static String RESOURCE = "DEMO1-" + Bukkit.getPluginManager().getPlugin("SpecializedCrates-Demo").getDescription().getVersion();
+
+    static String NONCE = "DEMO1-" + Bukkit.getPluginManager().getPlugin("SpecializedCrates-Demo").getDescription().getVersion();
 }
